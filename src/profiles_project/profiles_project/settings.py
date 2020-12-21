7d021d4cb38585ee5213d1630ad5a0345d0676fa
Dilmurod <dilmurod98@mail.ru>
2020-12-21 17:24:41 +0500

    Added a custom user profile moder and the mirgations

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = "profiles_api.UserProfile"
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -115,6 +117,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
