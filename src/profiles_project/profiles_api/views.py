3f3145ce7bab88c771238377aeb3980d66b26476
Dilmurod <dilmurod98@mail.ru>
2021-01-22 18:08:33 +0500

    Created first api viewp

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """Test api view"""
+
+    def get(self, request, format=None):
+        apiv = [
+            'Uses HTTP methods as function',
+            'Similar to Django view',
+            'Gives you most logic'
+        ]
+        return Response({'message': 'Hello', 'apiview': apiv})
