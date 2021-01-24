ef0615191e4e9758ad715ebe66bb316581453a2a
Dilmurod <dilmurod98@mail.ru>
2021-01-24 22:43:07 +0500

    Viewsets, Userprofile and basic permissions

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,65 @@
 from django.shortcuts import render
 
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializers
+from . import models
+from . import permissions
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles listing, creating and updating"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    
+
+class HelloViewSets(viewsets.ViewSet):
+    """Test api viewsets"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """return a hello message"""
+        ahello = [
+            'Users actions (list, create, retrieve, update, partial_update)',
+            'automatically maps to urls using routers',
+            'provides more func wiht less code'
+        ]
+        return Response({'message': 'Hello!', 'ahello': ahello})
+
+    def create(self, request):
+        """Create a new record"""
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """get by id"""
+        return Response({'method': 'GET (by id)'})
+
+    def update(self, request, pk=None):
+        """updating"""
+        return Response({'method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Update provided fields"""
+        return Response({'method': 'PATCH'})
+
+    def destroy(self, requst, pk=None):
+        """Delete"""
+        return Response({'method': 'DELETE'})
 
 
 class HelloApiView(APIView):
@@ -41,7 +96,7 @@ class HelloApiView(APIView):
         """Patch updates only field provided in the request"""
         return Response({'method': 'patch'})
 
-    def delete(self,request, pk=None):
+    def delete(self, request, pk=None):
         """Delete"""
 
         return Response({'method': 'delete'})
